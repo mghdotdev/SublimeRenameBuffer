46bd7032e2e329a2cf0bd23dfc477199072c56b5
Max G. Hegler <max@maxhegler.com>
2015-09-25 15:45:37 -0700

    dev

--- RenameTabLabel.py
+++ RenameTabLabel.py
@@ -2,26 +2,17 @@ import sublime
 import sublime_plugin
 
 class RenameTabLabelCommand(sublime_plugin.WindowCommand):
-	def run(self, text='', execute=False):
-		# if not self.window.active_view():
-		# 	return
+	def run(self, text='', **kwargs):
+		self.tab_index = kwargs['index']
+		self.tab_group = kwargs['group']
 		self.show_input_panel('Rename Tab Label:', text)
 
 	def on_done(self, text):
-		self.window.active_view().set_name(text)
+		if self.tab_group == -1 or self.tab_index == -1:
+			self.window.active_view().set_name(text)
+		else:
+			self.window.views_in_group(self.tab_group)[self.tab_index].set_name(text)
 
 	def show_input_panel(self, label, text):
-		print('label')
+		text = self.window.active_view().name();
 		view = self.window.show_input_panel(label, text, self.on_done, None, None)
-		# settings = view.settings()
-		# settings.set('is_widget', False)
-		# settings.set('gutter', False)
-		# settings.set('rulers', [])
-		# settings.set('spell_check', False)
-		# settings.set('word_wrap', False)
-		# settings.set('draw_minimap_border', False)
-		# settings.set('draw_indent_guides', False)
-		# settings.set('highlight_line', False)
-		# settings.set('line_padding_top', 0)
-		# settings.set('line_padding_bottom', 0)
-		# settings.set('auto_complete', False)
